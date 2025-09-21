4fd6774da8dee856d5422bbd3d317da4c2f05ddc
dev0jha <ojha6773@gmail.com>
2025-09-21 21:26:58 +0530

    add middleware and me routes

--- src/app/api/user/me/route.ts
+++ src/app/api/user/me/route.ts
@@ -1,4 +1,19 @@
+import { dbConnect } from "@/app/dbConfig/dbConfig";
 import { getDataFromToken } from "@/helper/getDataFromToken";
 import User from "@/models/userModel";
-import { NextRequest, NextResponse } from "next/server";
+import  { NextRequest, NextResponse } from "next/server";
 
+dbConnect();
+
+export const GET = async (request: NextRequest) => {
+    try {
+        const userId = await getDataFromToken(request);
+        const user = await User.findById({_id: userId}).select("-password");
+        return NextResponse.json({
+             message : "User fetched successfully",
+            data : user
+        });
+    } catch (error: any) {
+        return NextResponse.json({ error: error?.message ?? "Internal Server Error" }, { status: 500 });
+    }
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { toast } from "react-hot-toast";
+import { set } from "mongoose";
+import Link from "next/link";
 
 export default function ProfilePage() {
   const router = useRouter();
-
+  const [data, setData] = useState("nothing");
   const handleLogout = async () => {
     try {
       await axios.get("/api/user/logout");
@@ -18,18 +20,33 @@ export default function ProfilePage() {
       toast.error("Something went wrong during logout");
     }
   };
-
+ const getUserDetails =async() => {
+   const res=await axios.get("/api/user/me")
+      console.log(res.data);
+       setData(res.data.data._id);
+ }
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile Page</h1>
       <hr />
       <p>This is the profile page.</p>
+      <h2 className="p-1 rounded bg-amber-200">{data==='nothing'?"Nothing": <Link
+       href={`/profile/${data}`}>{data}
+        </Link>
+        }</h2>
       <hr />
       <button
         onClick={handleLogout}
         className="px-4 py-2 mt-4 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
       >
         Log Out
+      </button>
+
+      <button
+        onClick={getUserDetails}
+        className="px-4 py-2 mt-4 bg-amber-600 text-white rounded hover:bg-green-800 transition"
+      >
+        GetData
       </button>
     </div>
   );
