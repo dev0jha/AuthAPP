86cabd471b65a01686927fa971f430675ad529b0
dev0jha <ojha6773@gmail.com>
2025-09-22 16:57:03 +0530

    h

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,10 +1,11 @@
-export default function UserProfile({ params }: { params: { id: string } }) {
+export default async function UserProfile({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
   return(
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile Page</h1>
       <hr />
       <p className="text-4xl font-bold">This is the profile page
-        <span className="text-red-500 bg-amber-200 rounded-lg p-2 hover:bg-amber-800">{params.id}</span>
+        <span className="text-red-500 bg-amber-200 rounded-lg p-2 hover:bg-amber-800">{id}</span>
         </p>
 
     </div>
